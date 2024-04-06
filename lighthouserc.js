0d58dd431686e52cf72dfd45033dfae10b356c95
Adenilson <adenilson.oliveira1722@gmail.com>
2024-04-06 18:27:06 -0300

    Quarto Commit

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:accessibility': ['error', { 'minScore': 0.80 }]
+        'categories:accessibility': ['error', { 'minScore': 0.90 }]
       }
     },      
     upload: {
